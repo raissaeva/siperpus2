2c34d513b3ea6458b1d6c1d287ba17c467a679bd
raissaeva <63860092+raissaeva@users.noreply.github.com>
2020-04-19 09:40:02 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,60 +1,18 @@
-<?php
-include '../koneksi.php';
-
-$sql = "SELECT * FROM buku INNER JOIN kategori USING(id_kategori) ORDER BY judul";
-
-$res = mysqli_query($koneksi, $sql);
-
-$pinjam = array();
-
-
-while ($data = mysqli_fetch_assoc($res)) {
-    $pinjam[] = $data;
-}
-
-include '../aset/header.php';
-?>
-<center>
-    <div class="card">
-        <div class="card-header">
-            <h2 class="card-title"><i class="fas fa-edit"></i>Data buku</h2>
-        </div>
-        <a href="tambah.php"><button type="button" class="btn btn-info mt-3">TAMBAH DATA BUKU</button></a>
-        <div class="card-body">
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Judul</th>
-                        <th scope="col">pengarang</th>
-                        <th scope="col">Stok</th>
-                        <th scope="col">Aksi</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    $no = 1;
-                    foreach ($pinjam as $p) { ?>
-                        <tr>
-                            <th scope="row"><?= $no ?></td>
-                            <td><?= $p['judul'] ?></th>
-                            <td><?= $p['pengarang'] ?></td>
-                            <td><?= $p['stok'] ?></td>
-                            <td>
-                                <a href="detail.php?id=<?= $p['id_buku']; ?>" class="badge badge-success">Detail</a>
-                                <a href="edit.php?id=<?= $p['id_buku']; ?>" class="badge badge-warning">Edit</a>
-                                <a href="hapus.php?id=<?= $p['id_buku']; ?>" class="badge badge-danger">Hapus</a>
-                            </td>
-                        </tr>
-                    <?php
-                        $no++;
-                    }
-                    ?>
-                </tbody>
-            </table>
-        </div>
-    </div>
-</center>
-<?php
-include '../aset/footer.php';
-?>
+<form action="proses-login.php" method="post">
+    <table>
+        <tr>
+            <td><label for="">Username</label></td>
+            <td> <input type="text" name="username" id=""></td>
+        </tr>
+        <tr>
+            <td><label for="">Password</label></td>
+            <td> <input type="password" name="password" id=""></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>
+                <input type="submit" value="Login" name="btnlogin">
+            </td>
+        </tr>
+    </table>
+</form>
